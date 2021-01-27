f0131efc213827196b2a85e0aca2766ed32a7c14
qq378127636 <378127636@qq.com>
2021-01-27 18:05:05 +0800

    scloud

---
+++ scloud/service-ribbon/src/main/java/com/hxb/scloud/HelloService.java
@@ -0,0 +1,24 @@
+package com.hxb.scloud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    /**
+     * 负载均衡的方式调用服务
+     * @param name
+     * @return
+     */
+    public String hiService(String name) {
+        return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
+    }
+
+
+}
+
